6c62817434afba05a834b462086b2805e12a1136
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-04-12 16:32:36 +0530

    Added : UC2

--- src/com/bridgelabz/Github.java
+++ src/com/bridgelabz/Github.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class Github {
     public static void main(String[] args) {
-        System.out.println("Hello World from Bridgelabz");
+        System.out.println("Hi Guys");
     }
 }
